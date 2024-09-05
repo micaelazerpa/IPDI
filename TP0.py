16fc1cdc02addae1e4b5dd48f93b870222e56dc0
Emanuel Vilte <40330996@fi.unju.edu.ar>
2024-09-04 22:15:17 -0300

    Se agrega imagen como global

--- TP0.py
+++ TP0.py
@@ -1,8 +1,17 @@
 import tkinter as tk
-from tkinter import filedialog, Label, messagebox 
+from tkinter import filedialog, Label, messagebox
+import random as R
+from tkinter import *
+from tkinter import ttk
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import imageio
 from PIL import Image, ImageTk
 
 class Application(tk.Frame):
+    global url_image
+    url_image =""
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
@@ -22,15 +31,22 @@ class Application(tk.Frame):
         self.buttons_frame.pack(side="left", padx=10, pady=10)
 
         # Botón 1
-        self.button1 = tk.Button(self.buttons_frame, text="Botón 1", height=2, width=20)
+        self.button1 = tk.Button(self.buttons_frame, text="Guardar", height=2, width=20)
         self.button1.pack(pady=5)
 
         # Botón 2
-        self.button2 = tk.Button(self.buttons_frame, text="Botón 2", height=2, width=20)
+        self.button2 = tk.Button(self.buttons_frame, text="Procesar", height=2, width=20)
         self.button2.pack(pady=5)
-
+        saturacion='0'
+        saturaciones = ['10*10','20*20','50*50','100*100']
+        self.select= ttk.Combobox(self, textvariable=saturacion,values=saturaciones,height=12,width=20)
+        self.select.pack(pady=5)
+        self.select.set(saturacion)
+        self.seleccionado=tk.Label(self,textvariable=saturacion)
+        self.seleccionado.pack(pady=5)
         # Botón 3
-        self.button3 = tk.Button(self.buttons_frame, text="Botón 3", height=2, width=20)
+        self.button3 = tk.Button(self.buttons_frame, command=self.processImage,text="Botón RGB", height=2, width=20)
+        
         self.button3.pack(pady=5)
 
         # Cuadro 2 de 500x500 píxeles con borde de líneas de trazos
@@ -58,15 +74,16 @@ class Application(tk.Frame):
         self.out.pack(side="right", padx=10)
 
     def upload_image(self):
-        self.url_image = filedialog.askopenfilename(
+        url_image = filedialog.askopenfilename(
             title="Selecciona una imagen",
             filetypes=[("Archivos de imagen", "*.jpg;*.jpeg;*.png;*.bmp;*.gif")]
         )
-        if self.url_image: 
-            self.show_image(self.url_image)
+        if url_image: 
+            self.show_image(url_image)
 
     def show_image(self, url_image):
-        img = Image.open(url_image)
+        imgIO = imageio.imread(url_image)
+        img=Image.fromarray(imgIO)
         new_img = img.resize((500, 500))  # Cambiado para que la imagen se ajuste al tamaño del cuadro
         imagen_tk = ImageTk.PhotoImage(new_img)
         img1 = Label(self.square1, image=imagen_tk)
@@ -77,6 +94,29 @@ class Application(tk.Frame):
         response = messagebox.askquestion("Salir", "¿Desea salir de la interfaz?")
         if response == 'yes':
             self.master.destroy()
+    def processImage(self):
+        im = imageio.imread(url_image)
+        print(im.shape,im.dtype)
+
+        # we are working in float numbers [0,1] in image processing:
+
+        im = np.clip(im/255,0.,1.)
+        # print(im.shape,im.dtype)
+        YIQ=np.zeros(im.shape)
+        Y=im[:,:,0]
+        I=im[:,:,1]
+        Q=im[:,:,2]
+        YIQ[:,:,0] = np.clip((Y*0.299 +  I *0.587 + Q*0.114),0.,1.)
+        print(im.shape,im.dtype)
+        YIQ[:,:,1] = np.clip(Y*0.595 +  I *(-0.274) + Q*(-0.321),-0.59,0.59)
+        print(im.shape,im.dtype)
+        YIQ[:,:,2] = np.clip(Y*0.211 +  I *(-0.522) + Q*(0.311),-0.52,0.52)
+        # print(im.shape,im.dtype)
+
+
+        plt.imshow(YIQ[:,:,1],"gray")
+        plt.show()
+        
 
 root = tk.Tk()
 root.geometry('1300x800')  # Ajuste del tamaño de la ventana para acomodar los cuadros y botones
